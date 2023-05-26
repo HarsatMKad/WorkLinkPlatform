c115f7dc40f25396d663a3ba3e43bed317685932
Harsat <121182309+HarsatMKad@users.noreply.github.com>
2023-05-26 15:58:14 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace WorkLink
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.label1 = new System.Windows.Forms.Label();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.panel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.button1.BackColor = System.Drawing.Color.Honeydew;
+      this.button1.Location = new System.Drawing.Point(689, 21);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(151, 57);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Мое резюме";
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.button2.BackColor = System.Drawing.Color.DarkSeaGreen;
+      this.button2.Location = new System.Drawing.Point(846, 675);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(97, 47);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "Вперед";
+      this.button2.UseVisualStyleBackColor = false;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+      this.tableLayoutPanel1.ColumnCount = 1;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(28, 100);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 5;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(879, 569);
+      this.tableLayoutPanel1.TabIndex = 3;
+      this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint_1);
+      // 
+      // button3
+      // 
+      this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.button3.BackColor = System.Drawing.Color.RosyBrown;
+      this.button3.Location = new System.Drawing.Point(743, 675);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(97, 47);
+      this.button3.TabIndex = 4;
+      this.button3.Text = "Назад";
+      this.button3.UseVisualStyleBackColor = false;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // button4
+      // 
+      this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.button4.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+      this.button4.Location = new System.Drawing.Point(854, 21);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(101, 57);
+      this.button4.TabIndex = 5;
+      this.button4.Text = "Расписание собеседований";
+      this.button4.UseVisualStyleBackColor = false;
+      // 
+      // button5
+      // 
+      this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.button5.BackColor = System.Drawing.Color.LightSeaGreen;
+      this.button5.Location = new System.Drawing.Point(384, 25);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(279, 49);
+      this.button5.TabIndex = 6;
+      this.button5.Text = "Отклики";
+      this.button5.UseVisualStyleBackColor = false;
+      this.button5.Click += new System.EventHandler(this.button5_Click);
+      // 
+      // panel1
+      // 
+      this.panel1.Controls.Add(this.label1);
+      this.panel1.Controls.Add(this.comboBox1);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.panel1.Location = new System.Drawing.Point(0, 0);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(967, 734);
+      this.panel1.TabIndex = 7;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(25, 57);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(113, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Поиск по профессии";
+      // 
+      // comboBox1
+      // 
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Location = new System.Drawing.Point(28, 73);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(203, 21);
+      this.comboBox1.TabIndex = 0;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(967, 734);
+      this.Controls.Add(this.button5);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.panel1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.ComboBox comboBox1;
+    private System.Windows.Forms.Label label1;
+  }
+}
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkLink
+{
+  public partial class Form1 : Form
+  {
+    Vacancy v = new VacancyProgrammer();
+    public int key = 0;
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      this.Hide();
+      Form2 ewrg = new Form2();
+      ewrg.Show();
+    }
+
+    private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+    {
+      while (key < 5)
+      {
+        v.ShowVacancy(key, tableLayoutPanel1);
+        ++key;
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      tableLayoutPanel1.Controls.Clear();
+
+      if(key >= 20)
+      {
+        int key2 = key;
+        key -= 5;
+        while (key < key2)
+        {
+          v.ShowVacancy(key, tableLayoutPanel1);
+          ++key;
+        }
+        MessageBox.Show("конец списка");
+      }
+      else
+      {
+        int key2 = key + 5;
+        while (key < key2)
+        {
+          v.ShowVacancy(key, tableLayoutPanel1);
+          ++key;
+        }
+      }
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      tableLayoutPanel1.Controls.Clear();
+
+      if (key <= 5 )
+      {
+        int key2 = key;
+        key -= 5;
+        while (key < key2)
+        {
+          v.ShowVacancy(key, tableLayoutPanel1);
+          ++key;
+        }
+        MessageBox.Show("конец списка");
+      }
+      else
+      {
+        key -= 10;
+        int key2 = key + 5;
+        while (key < key2)
+        {
+          v.ShowVacancy(key, tableLayoutPanel1);
+          ++key;
+        }
+      }
+    }
+
+    private void button5_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
