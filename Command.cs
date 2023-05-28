f0dd76cc7b004193b11f0546e0438471ee0d3a1f
Harsat <121182309+HarsatMKad@users.noreply.github.com>
2023-05-28 10:03:05 +0700

    implemented command pattern

--- Command.cs
+++ Command.cs
@@ -1,46 +1,101 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WorkLink
 {
   class Receiver
   {
-    public void Operation()
+    Resume Resume;
+    public Resume ChangeResume(string LastNameFirstName, string Citizenship, string Address, string DateBirth, string Sex, string EducationInfo, long PhoneNumber, string Mail, long DesiredSalary, string Skills, string AdditionalInfo)
     {
-      Console.WriteLine("функция работает");
+      Resume = new Resume(LastNameFirstName, Citizenship, Address, DateBirth, Sex, EducationInfo, PhoneNumber, Mail, DesiredSalary, Skills, AdditionalInfo);
+      return Resume;
+    }
+
+    public Resume SaveResume()
+    {
+      JsonSerializer Serializer = new JsonSerializer();
+
+      using (StreamWriter Writer = new StreamWriter("Resume.json"))
+      {
+        JsonTextWriter JsonWriter = new JsonTextWriter(Writer) { CloseOutput = false };
+        Serializer.Serialize(JsonWriter, Resume);
+      }
+      return Resume;
+    }
+
+    public Resume CancelChanges()
+    {
+      string JsonFileName = "Resume.json";
+      Resume = JsonConvert.DeserializeObject<Resume>(File.ReadAllText(JsonFileName));
+      return Resume;
     }
   }
 
   abstract class Command
   {
-    public abstract void Execute();
+    public abstract Resume Execute(string LastNameFirstName, string Citizenship, string Address, string DateBirth, string Sex, string EducationInfo, long PhoneNumber, string Mail, long DesiredSalary, string Skills, string AdditionalInfo);
+
+    public abstract Resume Accept();
+
+    public abstract Resume Undo();
   }
 
   class ConcreteCommand : Command
   {
+    Resume Resume;
     Receiver receiver;
     public ConcreteCommand(Receiver r)
     {
       receiver = r;
     }
-    public override void Execute()
+    public override Resume Execute(string LastNameFirstName, string Citizenship, string Address, string DateBirth, string Sex, string EducationInfo, long PhoneNumber, string Mail, long DesiredSalary, string Skills, string AdditionalInfo)
     {
-      receiver.Operation();
+      Resume = receiver.ChangeResume(LastNameFirstName, Citizenship, Address, DateBirth, Sex, EducationInfo, PhoneNumber, Mail, DesiredSalary, Skills, AdditionalInfo);
+      return Resume;
+    }
+
+    public override Resume Accept()
+    {
+      Resume = receiver.SaveResume();
+      return Resume;
+    }
+
+    public override Resume Undo()
+    {
+      Resume = receiver.CancelChanges();
+      return Resume;
     }
   }
   class Invoker
   {
+    Resume Resume;
     Command command;
     public void SetCommand(Command c)
     {
       command = c;
     }
-    public void Run()
+    public Resume Run(string LastNameFirstName, string Citizenship, string Address, string DateBirth, string Sex, string EducationInfo, long PhoneNumber, string Mail, long DesiredSalary, string Skills, string AdditionalInfo)
     {
-      command.Execute();
+      Resume = command.Execute(LastNameFirstName, Citizenship, Address, DateBirth, Sex, EducationInfo, PhoneNumber, Mail, DesiredSalary, Skills, AdditionalInfo);
+      return Resume;
+    }
+
+    public Resume Accept()
+    {
+      Resume = command.Accept();
+      return Resume;
+    }
+
+    public Resume Undo()
+    {
+      Resume = command.Undo();
+      return Resume;
     }
   }
 }
